22706e418158055c857160d415c9da85dc242a24
xu561865 <xuyi561865@sina.com>
2016-05-24 00:53:45 +0800

    add sample2

---
+++ Sample2.h
@@ -0,0 +1,13 @@
+//
+//  Sample2.h
+//  Test_Algorithm
+//
+//  Created by 徐以 on 5/24/16.
+//  Copyright © 2016 xuyi. All rights reserved.
+//
+
+#ifndef Sample2_h
+#define Sample2_h
+
+
+#endif /* Sample2_h */
